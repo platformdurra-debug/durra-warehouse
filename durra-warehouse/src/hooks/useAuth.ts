d4afac0da31e0ba432297c5803f4531c684ea762
platformdurra-debug <platformdurra@gmail.com>
2026-05-30 22:47:46 +0300

    Delete durra-warehouse/src directory

--- durra-warehouse/src/hooks/useAuth.ts
+++
@@ -1,9 +0,0 @@
-"use client";
-import { useEffect } from "react";
-import { useAuthStore } from "@/store/authStore";
-
-export const useAuth = () => {
-  const { user, loading, error, login, register, logout, init } = useAuthStore();
-  useEffect(() => { init(); }, []);
-  return { user, loading, error, login, register, logout };
-};
